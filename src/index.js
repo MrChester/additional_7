fab0611ab689e20900a439b5dd13ea32d4641fba
Eugene Romankov <iromankov2010@gmail.com>
2018-03-07 16:29:32 +0300

    Add implementation of solveSudoku() and finding zeros coordinates in board

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
+module.exports = function solveSudoku(gameBoard) {
+    function zeroPositions(gameBoard) {
+        var zeroPositionsArr = [];
+
+        for (var i = 0; i < gameBoard.length; i++) {
+            for (var j = 0; j < gameBoard[i].length; j++) {
+                if (gameBoard[i][j] === 0) {
+                    zeroPositionsArr.push([i, j]);
+                }
+            }
+        }
+        console.log(zeroPositionsArr);
+        return zeroPositionsArr;
+    }
 }
